665d302890de85ba097bd4b3821ca113a47be423
Adabala-Dinesh <dadabala@neoterictechnologiesinc.com>
2024-10-10 10:15:51 +0530

    add text for jenkin test 10/10/24

--- src/main/java/com/neoteric/dockerlearning/springbootdocker/DockerConttroller.java
+++ src/main/java/com/neoteric/dockerlearning/springbootdocker/DockerConttroller.java
@@ -8,7 +8,7 @@ public class DockerConttroller {
     @GetMapping(value = "/docker")
 
     public String getDocker(){
-        System.out.println("JENKINS TEST");
+        System.out.println("JENKINS TEST today");
         return "Hello from docker and jenkins with webhooks ads  dinesh ";
     }
 }
